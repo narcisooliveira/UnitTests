06d8ff3865c3d1e55a08cdd012569c6985bc985d
Narciso Oliveira <narciso.oliveira@protechsolutions.com.br>
2024-09-27 10:43:56 -0300

    Add project files.

---
+++ UnitTests/Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace UnitTests.Tests;
+
+public static class WorldsDumbestFunctionTests
+{
+    [Fact]
+    public static void ReturnsPikachuIfZero_WhenValorIsZero_ReturnsPikachu()
+    {
+        try
+        {
+            // Arrange
+            var worldsDumbestFunction = new WorldsDumbestFunction();
+
+            // Act
+            var result = worldsDumbestFunction.ReturnsPikachuIfZero(0);
+
+            // Assert
+            Assert.Equal("Pikachu", result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    [Fact]
+    public static void ReturnsPikachuIfZero_WhenValorIsNotZero_ReturnsSquirtle()
+    {
+        try
+        {
+
+            // Arrange
+            var worldsDumbestFunction = new WorldsDumbestFunction();
+
+            // Act
+            var result = worldsDumbestFunction.ReturnsPikachuIfZero(1);
+
+            // Assert
+            Assert.Equal("Squirtle", result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ UnitTests/WorldsDumbestFunction.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTests;
+
+public class WorldsDumbestFunction
+{
+    public string ReturnsPikachuIfZero(int valor)
+    {
+        if (valor == 0)
+            return "Pikachu";
+
+        return "Squirtle";
+    }
+}
